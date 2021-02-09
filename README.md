7eeb9763614e732e02f1d9984c44b21f582edc9f
bborumoore <bborumoore@gmail.com>
2021-02-08 20:37:12 -0500

    I did some basic restructuring in the html file to adhere to filepath best practices.  I wrote the Javascript file to generate a password when the user hits the generate password button after running the user through a series of prompts.  Added placeholders to the readme for live URL and screenshot.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # password-generator
+The purpose of this application will be to generate a strong password using parameters selected by the User.
+
+The User is intially presented with a screen that holds a text box and a "Generate password" button. Upon clicking the button, the user will be guided through a series of prompts defining the parameters of the password (number of characters, uppercase, lowercase, etc).  
+
+Upon selecting the desired parameters, a password will be generated in the text box and be available for the user to copy.
+
+Here is a link for the deployed application:
+
+Here is a screenshot of the deployed application:
--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,75 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+
+};
+
+function generatePassword() {
+  let passLength = 0;
+
+  // First, we prompt the user for desired length of password, and ensure user input it within defined range. Stops function if user selects 'Cancel'
+  while(passLength < 16 || passLength > 128){
+    passLength = prompt("How many characters would you like in your password? (16-128 characters allowed): "); 
+    console.log(passLength);
+    if(passLength === null) {
+      return "No Password Generated";
+    } else if(passLength < 16 || passLength > 128) {
+        alert("Please enter a valid length \n(Somewhere between 16 and 128 characters)");        
+    }       
+  }
+
+  // Next, we run the user through a series of prompts to determine desired characters to utilize in password including:
+  // Lowercase? Uppercase? Numeric? Special?
+  let useLower = confirm("Would you like to use lowercase characters? Ok -> Yes, Cancel -> No");
+  let useUpper = confirm("Would you like to use UPPERCASE characters? Ok -> Yes, Cancel -> No");
+  let useNumeric = confirm("Would you like to use Numeric characters? Ok -> Yes, Cancel -> No");
+  let useSpecial = confirm("Would you like to use Special characters? Ok -> Yes, Cancel -> No");
+
+  // Set charactersets for different password parameters
+  allLower = "abcdefghijklmnopqrstuvwxyz";
+  allUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  allNumeric = "0123456789";
+  allSpecial = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+  // Next, define the characterset the password generator will use based on given parameters
+  let charSet = "";
+
+  if(useLower) {
+    charSet += allLower;
+  }
+
+  if(useUpper) {
+    charSet += allUpper;
+  }
+
+  if(useNumeric) {
+    charSet += allNumeric;
+  }
+
+  if(useSpecial) {
+    charSet += allSpecial;
+  }
+
+  console.log(charSet);
+  console.log(charSet.length);
+
+  // Finally, we generate a password using the given parameters.
+  let passHolder = "";
+  for(let i = passLength; i > 0; i--) {
+    passHolder += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  console.log(passHolder);
+
+  alert("We've generated you're password!  You'll find it in the text box below.");
+      return passHolder;
+  
 }
 
 // Add event listener to generate button
